af60885214c32bb2d54b64de74c010a0cc2b9004
My VO <kathy.nguyen198@gmail.com>
2020-07-04 10:38:18 +0200

    400 - erreur input: place_id + check_in

--- routes/bookings.js
+++ routes/bookings.js
@@ -5,6 +5,34 @@ const bookingsController = require('../controllers/bookings');
 const router = express.Router();
 
 router.post('/bookings', async (req, res) => {
+  const { place_id: placeId, check_in: checkIn } = req.body;
+
+  if (placeId === null || placeId === undefined || placeId === '') {
+    return res.status(400).json({
+      error: "Le champ place_id n'est pas renseigné",
+    });
+  }
+
+  if (Date.parse(checkIn).toString() === 'NaN') {
+    return res.status(400).json({
+      error:
+        'Le champ check_in doit être une chaîne de caractère correspondant à une date au format ISO 8601 sur le fuseau horaire UTC',
+    });
+  }
+
+  const dateParsed = new Date(Date.parse(checkIn));
+
+  // https://stackoverflow.com/questions/52869695/check-if-a-date-string-is-in-iso-and-utc-format
+  if (
+    dateParsed.toISOString() !== checkIn ||
+    dateParsed.toUTCString() !== new Date(checkIn).toUTCString()
+  ) {
+    return res.status(400).json({
+      error:
+        'Le champ check_in doit être une chaîne de caractère correspondant à une date au format ISO 8601 sur le fuseau horaire UTC',
+    });
+  }
+
   const newBooking = await bookingsController.addBooking(req.body);
 
   res.status(201).json({
