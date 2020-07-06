4e8c89d5d03483e7e505f6733c47bac93eb2f35f
Arthur Guez <Arthur@MacBook-Air-de-Arthur.local>
2020-07-06 17:10:28 +0200

    cors added

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ const server = require('./server');
 const PORT = 8080;
 
 server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}.`);
+  console.log(`CORS-enabled web server running on port ${PORT}.`);
 });
