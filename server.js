7aff490c5223fa60aa8047a43c2cf373d7a75ff2
Arthur Guez <Arthur@MacBook-Air-de-Arthur.local>
2020-07-06 17:10:28 +0200

    cors added

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const cors = require('cors');
+
 const router = require('./routes');
 
 const server = express();
 
 server.use('/api', router);
+server.use(
+  cors({
+    origin: 'http://localhost/',
+    methods: ['GET', 'POST', 'PATCH'],
+  })
+);
 
 module.exports = server;
