542e5f6e4e7e2dbb55616f2c6fc518bf4f2a800d
Arthur Guez <Arthur@MacBook-Air-de-Arthur.local>
2020-08-21 16:13:36 +0200

    fixed sequelize sync

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const db = require('./models/index');
 const server = express();
 
 // force: true will drop the table if it already exists
-db.sequelize.sync({ force: true }).then(() => {
-  console.log('Drop and Resync with { force: true }');
+db.sequelize.sync().then(() => {
+  console.log('Drop and Resync');
 });
 
 server.use(cors());
