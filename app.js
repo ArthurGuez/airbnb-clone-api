f4819a84e474727c42dcb38d92510441a8f84b46
Arthur Guez <Arthur@MacBook-Air-de-Arthur.local>
2020-08-24 10:45:31 +0200

    rm unecessary comments

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
 const server = require('./server');
-// const db = require('./models/index');
-
-// db.sequelize.sync().then(() => {
-//   console.log('Drop and Resync');
-// });
 
 const PORT = process.env.PORT || 8080;
 
