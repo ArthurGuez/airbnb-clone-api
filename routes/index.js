bc03f226b32fc2c6755c663c4621c175c7c0170b
Sylvie <sylvie.z.lu@gmail.com>
2020-08-26 14:16:24 +0200

    started errors handling

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,8 @@ const placesRouter = require('./places');
 const citiesRouter = require('./cities');
 const bookingsRouter = require('./bookings');
 
+const { OK } = require('../helpers/status_codes');
+
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 router.use(usersRouter);
@@ -15,13 +17,8 @@ router.use(citiesRouter);
 router.use(bookingsRouter);
 
 router.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello World!' });
-});
-
-router.get('*', (req, res) => {
-  res.status(404).json({
-    error: 'Vous vous êtes trompé de route !',
-  });
+  res.status(OK);
+  res.json({ message: 'Hello World!' });
 });
 
 module.exports = router;
