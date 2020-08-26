9dbaeed92c6d3eb15af605802f1f27a9a83a90f4
Sylvie <sylvie.z.lu@gmail.com>
2020-08-26 14:16:24 +0200

    started errors handling

--- server.js
+++ server.js
@@ -3,14 +3,26 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const express = require('express');
+const logger = require('morgan');
 const cors = require('cors');
 
 const router = require('./routes');
 
+const { errorLogger, errorHandler } = require('./middlewares');
+
 const server = express();
 
+const NotFoundError = require('./helpers/errors/not_found_error');
+
+server.use(logger('tiny'));
 server.use(cors());
 
 server.use('/api', router);
+server.use('*', (req, res, next) => {
+  throw new NotFoundError('Ressource introuvable', 'Vous vous êtes trompé de route !');
+});
+
+server.use(errorLogger);
+server.use(errorHandler);
 
 module.exports = server;
