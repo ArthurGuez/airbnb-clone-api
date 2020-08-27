ff76f01cbd7942ae6b79563d7a10eb18f2b50fae
Arthur Guez <Arthur@MacBook-Air-de-Arthur.local>
2020-08-27 14:18:45 +0200

    added handling of 500 error

--- middlewares/error_handler.js
+++ middlewares/error_handler.js
@@ -1,6 +1,13 @@
 module.exports = (error, req, res, next) => {
-  const { title, description } = error;
-  res.status(error.status).json({
+  const { title } = error;
+  let { description } = error;
+  const status = error.status || 500;
+
+  if (status === 500) {
+    description = 'Serveur cassé. Revenez plus tard.';
+  }
+
+  res.status(status).json({
     title,
     description,
   });
